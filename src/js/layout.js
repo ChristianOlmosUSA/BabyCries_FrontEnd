15ff536ccf20848810465c24f756ebcf3b6ed1e3
Chris <christianOlmos@protonmail.com>
2020-06-27 01:52:15 +0000

    added create new account Page

--- src/js/layout.js
+++ src/js/layout.js
@@ -9,6 +9,7 @@ import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import { Settings } from "./views/settings";
 import { HistorySingleDay } from "./views/historySingleDay";
+import { CreateNewAccount } from "./views/createNewAccount";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -31,7 +32,8 @@ export const Layout = () => {
 						<Route path="/login" component={Login} />
 						<Route path="/video" component={Video} />
 						<Route path="/settings" component={Settings} />
-						<Route path="/historySingleDay" component={HistorySingleDay} />
+						<Route path="/historysingleday" component={HistorySingleDay} />
+						<Route path="/createnewaccount" component={CreateNewAccount} />
 						<Route path="/single/:theid" component={Single} />
 						<Route render={() => <h1>Not found!</h1>} />
 					</Switch>
