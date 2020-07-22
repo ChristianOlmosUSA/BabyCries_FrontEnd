840f25f1029cc6bff394603331a67518e625ca17
John Rohan <john.rohan87@gmail.com>
2020-07-22 03:52:48 +0000

    updating

--- src/js/views/historyList.js
+++ src/js/views/historyList.js
@@ -1,31 +1,31 @@
-import React from "react";
-//import moment from "moment";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import moment from "moment";
 import "../../styles/historyList.scss";
 /* THIS IS REALLY A DRAFT OF A DRAFT! dont want a class, need to loop through props for all history */
+let entries = [];
 
 export const HistoryList = () => {
 	{
-		/*const [endDate, setEndDate] = React.useState(moment().subtract(7, "day"));
-	let current = moment();
+		const { actions, store } = useContext(Context);
+		const [endDate, setEndDate] = useState(moment().subtract(7, "day"));
+		const [someData, setSomeData] = useState(null);
 
-	let entries = [];
-	while (endDate.isBefore(current)) {
-		entries.push(moment(current));
-		current = moment(current).subtract(1, "day");
-	}
-	console.log(entries);
-*/
+		useEffect(() => {
+			let current = moment();
+
+			while (endDate.isBefore(current)) {
+				entries.push(moment(current));
+				current = moment(current).subtract(1, "day");
+			}
+			console.log("entries " + entries);
+		}, []);
 	}
 	return (
 		<div className="text-center container bg-white text-center">
 			<div>History List</div>
-			{entries.map((e, i) => {
-				return (
-					<div key={i} type="button" className="history-entry">
-						{e.format("Do MMM, YYYY")}
-					</div>
-				);
-			})}
+			{console.log(entries)}
+			{console.log(Object.values(entries).forEach(item => entries[item]))}
 			<div
 				type="button"
 				className="history-entry"
