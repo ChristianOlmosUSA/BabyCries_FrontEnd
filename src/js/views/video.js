ee77d1745bcd86a8fb53217fcfbf6031c8dc9f10
Chris <christianOlmos@protonmail.com>
2020-07-26 05:41:36 +0000

    setting up iframe for video streaming

--- src/js/views/video.js
+++ src/js/views/video.js
@@ -1,12 +1,23 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import Iframe from "react-iframe";
 
 export const Video = () => (
 	<div className="text-center backdrop">
 		<h1>Live Video Here!</h1>
 		<dir className="m-5">
-			<img className="shadow-lg" src="https://via.placeholder.com/640x480" />
+			<Iframe
+				url="http://192.168.1.76/stream.mjpg"
+				// url="https://www.youtube.com/embed?v=5ECD8J3dvDQ"
+				width="640px"
+				height="480px"
+				id="ImpossibleStringIDkaybvkurycbucvehcbkewucxyrfewqxctv26427f6gx"
+				className="theLiveVideo"
+				display="initial"
+				position="relative"
+				allowFullScreen
+			/>
 		</dir>
 	</div>
 );
