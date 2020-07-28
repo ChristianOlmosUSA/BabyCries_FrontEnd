a0d8378a9990da61523c94a8c3d5c22be645f2e0
John Rohan <john.rohan87@gmail.com>
2020-07-28 19:33:11 +0000

    updating

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,9 +9,9 @@ export const Navbar = () => {
 	const [babyName, setBabyName] = useState("Magic Monitor");
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand" href="#">
-				Navbar
-			</a>
+			<Link className="nav-brand" to="./">
+				Magic Monitor
+			</Link>
 			<button
 				className="navbar-toggler"
 				type="button"
@@ -25,52 +25,51 @@ export const Navbar = () => {
 
 			<div className="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul className="navbar-nav mr-auto">
-					<li className="nav-item active">
-						<a className="nav-link" href="#">
-							Home <span className="sr-only">(current)</span>
-						</a>
+					<li className="nav-item">
+						<Link className="nav-link" to="./">
+							HOME
+						</Link>
 					</li>
 					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Link
-						</a>
+						<Link className="nav-link" to="/about">
+							ABOUT US
+						</Link>
 					</li>
-					<li className="nav-item dropdown">
-						<a
-							className="nav-link dropdown-toggle"
-							href="#"
-							id="navbarDropdown"
-							role="button"
-							data-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false">
-							Dropdown
-						</a>
-						<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a className="dropdown-item" href="#">
-								Action
-							</a>
-							<a className="dropdown-item" href="#">
-								Another action
-							</a>
-							<div className="dropdown-divider" />
-							<a className="dropdown-item" href="#">
-								Something else here
-							</a>
-						</div>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link disabled" href="#">
-							Disabled
-						</a>
-					</li>
+					{store.token ? (
+						<>
+							<li>
+								<Link className="nav-link" to="/video">
+									LIVE VIDEO
+								</Link>
+							</li>
+							<li>
+								<Link className="nav-link" to="/dashboard">
+									DASHBOARD
+								</Link>
+							</li>
+							<li>
+								<Link className="nav-link" to="/historylist">
+									PREVIOUS SLEEPS
+								</Link>
+							</li>
+						</>
+					) : (
+						""
+					)}
+					{store.token ? (
+						<li>
+							<Link className="nav-link" to="/login">
+								LOG OUT - NOT IMPLEMENTED
+							</Link>
+						</li>
+					) : (
+						<li>
+							<Link className="nav-link" to="/login">
+								LOGIN
+							</Link>
+						</li>
+					)}
 				</ul>
-				<form className="form-inline my-2 my-lg-0">
-					<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-					<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-						Search
-					</button>
-				</form>
 			</div>
 		</nav>
 	);
