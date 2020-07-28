34fd2e86c6e6c8dc07f2773cfb8929dce1c2859e
John Rohan <john.rohan87@gmail.com>
2020-07-28 00:17:05 +0000

    updating

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -18,27 +18,22 @@ export const About = () => (
 						</p>
 					</div>
 				</div>
-				<div className="mx-auto col flex-wrap">
-					<p className="w-100">
-						<img className="homeImage img-fluid" src={rigoImage} />
-					</p>
-				</div>
 			</div>
 		</div>
 
 		<div className="p5 mt-3 flex-col">
 			<div className="">
-				<h2>How Magic Monitor Works</h2>
+				<h2>Magic Monitor: <i>The Product</i></h2>
 			</div>
 			<div className="p5 mt-3">
-				<div className="">Using artificial intelligence from Google</div>
-				<div className="">Every breath is counted and timed</div>
-				<div className="">her face position is checked every second</div>
-				<div className="">and his every cry is compared to a database of 700 baby cries</div>
-				<div className="">explained by reasons such as pain, hunger, diaper change, discomfort</div>
-				<div className="">
-					<small>* no images or sounds are stored by us, and you have complete control over the data</small>
-				</div>
+				<div className="">Magic Monitor offers 3 new distinct functions no other baby monitor offers – especially from camera only product.​</div>
+				<ol>
+                    <li className="">Detects whether the baby is face up or face down. Most international guidelines suggest young babies are kept face up​ to prevent SIDS.</li>
+                    <li className="">Identifies the breathing rate of the baby. The user gets default variables, and can also adjust these or disable the alert. But the great thing about these two functions is getting an alert to your phone by text message telling you if your baby has very rapid or slow breathing or has rolled onto their face​</li>
+                    <li className="">Cry Interpretation. Using an opensource database of 715 cries which have been categorized, we can also respond to the noise of crying whether it is ‘hunger’, ‘sleepiness’, ’boredom’, ’colicky’, ’sick’, or ’has had enough’ ​​</li>
+                </ol>
+                
+				<div className="">As with any standard baby monitor there is a live streaming video page. The security of this is important – each monitor would arrive with a unique 32 digit key used for the first sign-up linking to a unique subdomain, on top of the JWT secure login. No video is saved (But a future option might be a play-back)</div>
 			</div>
 			<div className="container homePageLogo">
 				<div className="d-flex flex-row justify-content-around w-50 ml-3">
