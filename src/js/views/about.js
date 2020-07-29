72089f5dc7e6b24d6e364fde58986079204e8289
John Rohan <john.rohan87@gmail.com>
2020-07-29 23:55:19 +0000

    updating

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -28,7 +28,7 @@ export const About = () => (
 		<div className="card-group container-fluid">
 			<div className="container card-body" style={{ maxWidth: "24rem" }}>
 				<h3>Chris Olmos</h3>
-				<div className="p-2">
+				<div className="">
 					<div className="m-2">Economics Degree, 2003</div>
 					<div className="m-2">20 years in banking and business owner​</div>
 					<div className="m-2">Quit work to full time on inventions for the next 2 years​</div>
@@ -37,7 +37,7 @@ export const About = () => (
 			</div>
 			<div className="container card-body" style={{ maxWidth: "24rem" }}>
 				<h3>John Rohan</h3>
-				<div className="p-2">
+				<div className="">
 					<div className="m-2">Self-taught in multiple computer langusges</div>
 					<div className="m-2">15 years of freelance development and consulting​</div>
 					<div className="m-2">4 years of resturant managment in Brikell, Miami​</div>
